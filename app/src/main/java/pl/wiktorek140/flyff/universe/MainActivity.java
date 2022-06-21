8394d51429be7a19fc74647d137865643fbd18fd
Wiktor Strzębała <wiktorek140@gmail.com>
2022-06-21 16:35:21 +0200

    Add files via upload

--- app/src/main/java/pl/wiktorek140/flyff/universe/MainActivity.java
+++ app/src/main/java/pl/wiktorek140/flyff/universe/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.*;
 import android.widget.*;
 import android.graphics.*;
 import android.widget.RelativeLayout.*;
+import ren.yale.android.cachewebviewlib.*;
+import android.annotation.*;
 
 public class MainActivity extends Activity 
 {
@@ -17,14 +19,30 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		getWindow().setFlags(WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES | WindowManager.LayoutParams.FLAG_FULLSCREEN,
+		getWindow().setFlags(WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES ,
 							 WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES | WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		
 		setContentView(R.layout.main);
    
-		
-		mWebView=(WebView)findViewById(R.id.webview);
-		mWebView.setWebViewClient(new WebViewClient());
+		WebViewCacheInterceptorInst.getInstance().
+			init(new WebViewCacheInterceptor.Builder(this));
+			
+		mWebView = findViewById(R.id.webview);
+		mWebView.setWebViewClient(new WebViewClient(){
+
+				@TargetApi(Build.VERSION_CODES.LOLLIPOP)
+				@Nullable
+				@Override
+				public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
+					return  WebViewCacheInterceptorInst.getInstance().interceptRequest(request);
+				}
+@Nullable
+            @Override
+            public WebResourceResponse shouldInterceptRequest(WebView view, String url) {
+                return  WebViewCacheInterceptorInst.getInstance().interceptRequest(url);
+            }
+			});
+	
 		mWebView.setWebChromeClient(new MyChrome());
 		
 		WebSettings webSettings = mWebView.getSettings();
@@ -32,8 +50,14 @@ public class MainActivity extends Activity
 		webSettings.setAppCacheEnabled(true);
 		webSettings.setDomStorageEnabled(true);
 		webSettings.setAllowFileAccess(true);
+		webSettings.setAllowContentAccess(true);
+		webSettings.setAppCachePath(this.getCacheDir().getAbsolutePath());
+        webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
+		//webSettings.setAppCacheMaxSize(Long.MAX_VALUE);
+		
 		
 		mWebView.loadUrl("https://universe.flyff.com/play");
+		return;
     }
 	
 	private class MyChrome extends WebChromeClient {
@@ -49,11 +73,12 @@ public class MainActivity extends Activity
         public void onHideCustomView()
         {
             ((FrameLayout)getWindow().getDecorView()).removeView(this.mCustomView);
-            this.mCustomView = null;
+            this.mCustomView = (View) null;
             getWindow().getDecorView().setSystemUiVisibility(this.mOriginalSystemUiVisibility);
             setRequestedOrientation(this.mOriginalOrientation);
             this.mCustomViewCallback.onCustomViewHidden();
-            this.mCustomViewCallback = null;
+            this.mCustomViewCallback = (WebChromeClient.CustomViewCallback) null;
+			return;
         }
 
         public void onShowCustomView(View paramView, WebChromeClient.CustomViewCallback paramCustomViewCallback)
@@ -67,19 +92,19 @@ public class MainActivity extends Activity
             this.mOriginalSystemUiVisibility = getWindow().getDecorView().getSystemUiVisibility();
             this.mOriginalOrientation = getRequestedOrientation();
             this.mCustomViewCallback = paramCustomViewCallback;
-            ((FrameLayout)getWindow().getDecorView()).addView(this.mCustomView, new FrameLayout.LayoutParams(-1, -1));
 			getWindow().setFlags(WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES | WindowManager.LayoutParams.FLAG_FULLSCREEN,
 								 WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES | WindowManager.LayoutParams.FLAG_FULLSCREEN);
+			
+			
+			((FrameLayout)getWindow().getDecorView()).addView(this.mCustomView, new FrameLayout.LayoutParams(-1, -1));
 			
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
 															 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
 															 | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
 															 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
 															 | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-															 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-															 
-														 
-	
+															 | View.SYSTEM_UI_FLAG_IMMERSIVE);
+		return;
         }
     }
 	
@@ -87,12 +112,20 @@ public class MainActivity extends Activity
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         mWebView.saveState(outState);
+		return;
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
         mWebView.restoreState(savedInstanceState);
+		return;
     }
+	
+	@Override
+	public void onBackPressed() {
+        this.moveTaskToBack(true);
+		return;
+	}
 	
 }
